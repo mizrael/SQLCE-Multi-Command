76a3b4f11bd7d9081c5314538247d76694e42e4f
Davide Guida <mizrael@yahoo.it>
2015-01-05 16:11:50 +0100

    Update example.cs

--- example.cs
+++ example.cs
@@ -1,58 +1,59 @@
 private System.Data.DataSet ExecuteScript(string scriptPath)
-        {
-            var lines = ReadScript(scriptPath);
+{
+	var lines = ReadScript(scriptPath);
 
-            var ds = new System.Data.DataSet();
+	var ds = new System.Data.DataSet();
 
-            var connString = ConfigurationManager.ConnectionStrings["my_conn_string"].ConnectionString;
+	var connString = ConfigurationManager.ConnectionStrings["my_conn_string"].ConnectionString;
 
-            using (var conn = new SqlCeConnection(connString))
-            {
-                conn.Open();
+	using (var conn = new SqlCeConnection(connString))
+	{
+		conn.Open();
 
-                var sb = new StringBuilder();
+		var sb = new StringBuilder();
 
-                foreach (var line in lines)
-                {
-                    var currLine = line.Trim();
+		foreach (var line in lines)
+		{
+			var currLine = line.Trim();
 
-                    if (currLine.Equals("GO", StringComparison.OrdinalIgnoreCase))
-                    {
-                        FillDataset(sb.ToString(), conn, ds);
-                        sb.Remove(0, sb.Length);
-                    }
-                    else
-                    {
-                        sb.Append(currLine);
-                        sb.Append(Environment.NewLine);
-                    }
-                }
+			if (currLine.Equals("GO", StringComparison.OrdinalIgnoreCase))
+			{
+				FillDataset(sb.ToString(), conn, ds);
+				sb.Remove(0, sb.Length);
+			}
+			else
+			{
+				sb.Append(currLine);
+				sb.Append(Environment.NewLine);
+			}
+		}
 
-                if (sb.Length != 0)
-                    FillDataset(sb.ToString(), conn, ds);
-            }
+		if (sb.Length != 0)
+			FillDataset(sb.ToString(), conn, ds);
+	}
 
-            return ds;
-        }
+	return ds;
+}
 
-        private IEnumerable<string> ReadScript(string scriptPath) {
-            var basePath = Server.MapPath("~/App_Data/");
-            var scriptServerPath = System.IO.Path.Combine(basePath, scriptPath);
-            if (!System.IO.File.Exists(scriptServerPath))
-                return Enumerable.Empty<string>();
+private IEnumerable<string> ReadScript(string scriptPath) 
+{
+	var basePath = Server.MapPath("~/App_Data/");
+	var scriptServerPath = System.IO.Path.Combine(basePath, scriptPath);
+	if (!System.IO.File.Exists(scriptServerPath))
+		return Enumerable.Empty<string>();
 
-            return System.IO.File.ReadAllLines(scriptServerPath);
-        }
+	return System.IO.File.ReadAllLines(scriptServerPath);
+}
 
-        private static System.Data.DataSet FillDataset(string query, SqlCeConnection conn, System.Data.DataSet ds)
-        {
-            using (var cmd = conn.CreateCommand())
-            {
-                cmd.CommandText = query;
-                cmd.CommandType = System.Data.CommandType.Text;
-                var adapter = new SqlCeDataAdapter(cmd);
-                adapter.Fill(ds);
-            }
+private static System.Data.DataSet FillDataset(string query, SqlCeConnection conn, System.Data.DataSet ds)
+{
+	using (var cmd = conn.CreateCommand())
+	{
+		cmd.CommandText = query;
+		cmd.CommandType = System.Data.CommandType.Text;
+		var adapter = new SqlCeDataAdapter(cmd);
+		adapter.Fill(ds);
+	}
 
-            return ds;
-        }
+	return ds;
+}
